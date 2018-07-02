28bfc03487f8e26c45e4428302104c19459e49ad
Fritz Jay <jonathan.jay10@gmail.com>
2018-07-01 23:24:35 -0700

    Created TestArgumentError to easily format error messages

---
+++ src/library/errors.ts
@@ -0,0 +1,9 @@
+export class TestArgumentError extends Error {
+  constructor(name: string, value: any, additionalMessage?: string) {
+    super();
+    this.message = `Invalid value for argument \`${name}\`: "${value}"`;
+    if (additionalMessage) {
+      this.message += ` ${additionalMessage}`;
+    }
+  }
+}
