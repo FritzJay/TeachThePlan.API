ad47cf40b981b4df956103c3a9f7171d5abd606b
Fritz Jay <jonathan.jay10@gmail.com>
2018-07-02 23:13:26 -0700

    Changed mongodb connection string

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as config from './config';
 var app = express();
 import { urlencoded, json } from 'body-parser';
 import { connect } from 'mongoose';
@@ -7,7 +8,7 @@ import { testsRouter } from './src/routes/tests.route';
 
 const PORT: number = 3000;
 
-connect('mongodb://localhost/jwtauth');
+connect(`mongodb://factfluency:${config.mongodbPassword}@factfluency-shard-00-00-vywlr.mongodb.net:27017,factfluency-shard-00-01-vywlr.mongodb.net:27017,factfluency-shard-00-02-vywlr.mongodb.net:27017/test?ssl=true&replicaSet=FactFluency-shard-0&authSource=admin&retryWrites=true`);
 
 app.use(urlencoded({ extended: false }));
 app.use(json());
