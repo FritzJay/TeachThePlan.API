ac5d6d24959ca0f8565db412769aab261be3571f
Fritz Jay <jonathan.jay10@gmail.com>
2018-07-21 11:27:40 -0700

    Added cors to server

--- server.ts
+++ server.ts
@@ -12,10 +12,18 @@ import { studentRouter } from './src/routes/students.route';
 
 const PORT: number = 3000;
 
+const allowCrossDomain = (require, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+}
+
 connect(`mongodb://factfluency:${config.mongodbPassword}@factfluency-shard-00-00-vywlr.mongodb.net:27017,factfluency-shard-00-01-vywlr.mongodb.net:27017,factfluency-shard-00-02-vywlr.mongodb.net:27017/test?ssl=true&replicaSet=FactFluency-shard-0&authSource=admin&retryWrites=true`);
 
 app.use(urlencoded({ extended: false }));
 app.use(json());
+app.use(allowCrossDomain);
 app.use('/user', userRouter);
 app.use('/tests', testsRouter);
 app.use('/schools', schoolsRouter);
