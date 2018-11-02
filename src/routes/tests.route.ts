b0fa983d440a503ec8c1bde104360848444ee63c
Fritz Jay <jonathan.jay10@gmail.com>
2018-11-01 20:22:00 -0700

    updated tests route to use async await

--- src/routes/tests.route.ts
+++ src/routes/tests.route.ts
@@ -1,36 +1,35 @@
-import { Router, Request, Response } from 'express';
-import { getAvailableTests, newTest, gradeTest, IAvailableTests, submitTest, ITestResults } from '../library/tests/tests';
-import { ITestParameters, getTestParameters } from '../library/testParameters/testParameters';
-import { ITest, IQuestion } from '../library/tests/tests';
-import { authorizeUser } from '../library/authentication/authentication';
+import { Router, Request, Response } from 'express'
+import { getAvailableTests, newTest, gradeTest, submitTest } from '../library/tests/tests'
+import { getTestParameters } from '../library/testParameters/testParameters'
+import { IQuestion } from '../library/tests/tests'
+import { authorizeUser } from '../library/authentication/authentication'
 
-export let testsRouter = Router();
+export let testsRouter = Router()
 
 /*
   Return a list of available tests
 
   Authorization: student
 */
-testsRouter.get('/available', (request: Request, response: Response) => {
-  authorizeUser(request.headers.authorization, 'student')
-  .then((user) => {
-    getAvailableTests(user._id)
-    .then((availableTests: IAvailableTests) => {
-      response.status(200).json({
-        availableTests: availableTests
-      });
+testsRouter.get('/available', async (request: Request, response: Response) => {
+  try {
+    const { _id } = await authorizeUser(request.headers.authorization, 'student')
+  
+    const availableTests = await getAvailableTests(_id)
+    
+    response.status(200).json({
+      success: 'Available tests were found',
+      availableTests
     })
-    .catch((error) => {
-      response.status(401).json({
-        error: error.toString()
-      });
-    });
-  }).catch((error) => {
-    response.status(401).json({
+  }
+
+  catch (error) {
+    console.log('Error ocurred in tests/available', error)
+    response.status(500).json({
       error: error.toString()
-    });
-  });
-});
+    })
+  }
+})
 
 /*
   Returns a new test with empty questions
@@ -50,18 +49,23 @@ testsRouter.post('/new', async (request: Request, response: Response) => {
     const { _id } = await authorizeUser(request.headers.authorization, 'student')
 
     const testParameters = await getTestParameters(_id)
-    testParameters.operator = request.body.operator;
-    testParameters.number = request.body.number;
+    testParameters.operator = request.body.operator
+    testParameters.number = request.body.number
 
     const test = await newTest(testParameters, _id)
-    response.status(200).json({ test })
+
+    response.status(200).json({
+      success: 'Test was created',
+      test,
+    })
+
   } catch (error) {
     console.log('Error ocurred during test/new', error)
     response.status(500).json({
       error: error.toString()
     })
   }
-});
+})
 
 /*
   Returns a graded version of a given test
@@ -75,40 +79,36 @@ testsRouter.post('/new', async (request: Request, response: Response) => {
     questions
   }
 */
-testsRouter.post('/grade', (request: Request, response: Response) => {
-  authorizeUser(request.headers.authorization, 'student')
-  .then((user) => {
-    const test: ITest = {
-      userID: user.id,
-      duration: request.body.duration,
-      start: new Date(request.body.start),
-      end: new Date(request.body.end),
+testsRouter.post('/grade', async (request: Request, response: Response) => {
+  try {
+    const { duration, start, end } = request.body
+
+    const { _id } = await authorizeUser(request.headers.authorization, 'student')
+
+    const test = {
+      userID: _id,
+      duration: duration,
+      start: new Date(start),
+      end: new Date(end),
       questions: getQuestionsFromRequest(request),
     }
-    gradeTest(test)
-    .then((testResults: ITestResults) => {
-      submitTest(test)
-      .then((_test: ITest) => {
-        return response.status(200).json({testResults});
-      })
-      .catch((error) => {
-        response.status(500).json({
-          error: error.toString()
-        });
-      });
+
+    const testResults = await gradeTest(test)
+
+    await submitTest(test)
+
+    response.status(200).json({
+      success: 'Test was submitted',
+      testResults,
     })
-    .catch((error) => {
-      response.status(500).json({
-        error: error.toString()
-      });
-    });
-  })
-  .catch((error) => {
-    response.status(401).json({
+  
+  } catch (error) {
+    console.log('Error ocurred in tests/grade', error)
+    response.status(500).json({
       error: error.toString()
-    });
-  });
-});
+    })
+  }
+})
 
 const getQuestionsFromRequest = (request: Request): IQuestion[] => {
   return request.body.questions.map((q): IQuestion => {
@@ -118,5 +118,5 @@ const getQuestionsFromRequest = (request: Request): IQuestion[] => {
       start: new Date(q.start),
       end: new Date(q.end)
     }
-  });
+  })
 }
